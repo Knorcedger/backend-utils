2128874cab490d6f593336e2ccd0a1ef11e78681
Achilleas Tsoumitas <knorcedger@gmail.com>
2022-07-06 15:52:44 +0000

    Export sendEmailGun

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from './middlewares/logSetup';
 export * from './utils/catchAppErrors';
 export * from './utils/optionallyAddAttrs';
 export * from './utils/pagination';
+export * from './utils/sendEmailGun';
